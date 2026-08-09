672b6b7c76c7a54ddfb4dbee724b91d92f2df9fb
shifad878-prog <shifad878@gmail.com>
2026-08-09 14:08:42 +0500

    Done

---
+++ app.js
@@ -0,0 +1,45 @@
+const supabaseUrl = "https://gvmyyjvbrmgmwgczqupm.supabase.co";
+const supabaseKey = "sb_publishable_c3H02qaCKQ8DlLveDEvIHA_4H5_nKpE";
+
+const { createClient } = supabase
+
+const client = createClient(supabaseUrl, supabaseKey);
+
+console.log(client);
+
+let submitBtn = document.querySelector("#btn");
+let studentName = document.querySelector("#name");
+let studentCourse = document.querySelector("#course");
+let studentEmail = document.querySelector("#email");
+
+submitBtn.addEventListener("click", async (event) => {
+    try {
+        event.preventDefault();
+
+        if (!studentName.value || !studentCourse.value || !studentEmail.value) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Fill all data",
+            });
+            return;
+        }
+        const { error } = await client
+            .from('student_data')
+            .insert([{
+                name : studentName.value,
+                course : studentCourse.value,
+                email : studentEmail.value,
+            }])
+            console.log(error)
+        Swal.fire({
+            title: "Added Successfully!",
+            icon: "success",
+            draggable: true
+        });
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
